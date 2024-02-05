04dade60252e9e2af4a70b2476c46396ef013240
Basil <i.najar96@gmail.com>
2024-02-05 16:22:25 -0500

    Implement registration page

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,49 @@
 import express from "express";
-import db from "../database/postgres.database.js";
+import pg from "pg";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const loginRouter = express.Router();
+const { Pool } = pg;
 
-loginRouter.get("/login", (req, res) => {
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+  user: process.env.PG_USERNAME,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+app.post("/auth", async (req, res) => {});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "../views/register.html");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password, email } = req.body;
   console.log(req.body);
+
+  try {
+    const client = await pool.connect();
+    const result = await client.query(
+      "INSERT INTO users (username, password, email) VALUES ($1, $2, $3)",
+      [username, password, email]
+    );
+
+    res.redirect("/login");
+    client.release();
+  } catch (error) {
+    console.error("Error during registration:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
